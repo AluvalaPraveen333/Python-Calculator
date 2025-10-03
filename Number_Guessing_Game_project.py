446e0500a3da33efb13c1f36cbdf05d384a61607
AluvalaPraveen333 <aluvalapraveen333@gmail.com>
2025-10-03 20:08:28 +0530

    Add files via upload

---
+++ Number_Guessing_Game_project.py
@@ -0,0 +1,29 @@
+
+# === Python project: Number Guessing Game === #
+
+import random
+
+number = random.randint(1,100)
+attempts = 0
+max_attempts = 7
+
+print("Guess the number (1,100)")
+
+while attempts < max_attempts:
+    try:
+        guess = int(input(f"Attempt {attempts+1}: Enter your guess:"))
+        attempts += 1
+
+        if guess == number:
+            print("Correct! you guessed it.")
+            break
+        elif guess < number:
+            print("Too Low!")
+        else:
+            print("Too High!")
+
+    except ValueError:
+        print("Please enter a valid number.")
+
+if attempts == max_attempts and guess != number:
+    print(f"Out of attempts! The number was {number}.")
